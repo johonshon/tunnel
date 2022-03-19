efa8ccb991dfb16c1eeb62ecc4a6ddb667024fbe
johonshon <101489888+johonshon@users.noreply.github.com>
2022-03-19 15:43:51 +0800

    Update installer.sh

--- instalr/installer.sh
+++ instalr/installer.sh
@@ -13,20 +13,20 @@ exit 0
 else
 clear
 echo " "
-printf "              \e[1;91m Turn On Internet Please....!\e[0m\n"
+printf "               Turn On Internet Please....!"
 echo " "
 cd $HOME/tunnel/do
 bash do.sh
 fi
 echo ' '
-printf "              \e[1;92m Ngrok installed and starting....!\e[0m\n"
+printf "               Ngrok installed and starting....!"
 echo ' '
 sleep 3.0
 clear
 else
 clear
 echo " "
-printf "              \e[1;91m Ngrok Is Installing Wait..../\e[0m\n"
+printf "               Ngrok Is Installing Wait..../"
 echo " "
 sleep 3.0
 clear
@@ -34,7 +34,6 @@ cd $HOME
 pkg install wget -y
 pkg install php -y
 pkg install python -y
-pip install lolcat
 pkg install mpv -y
 wget --no-check-certificate https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip
 unzip ngrok-2.2.8-linux-arm64.zip
@@ -42,26 +41,11 @@ chmod +x ngrok
 rm -rf ngrok-2.2.8-linux-arm64.zip
 clear
 echo " "
-printf "                  \e[1;91m Ngrok os Starting....!\e[0m\n"
+printf "                  Ngrok os Starting....!"
 echo " "
 sleep 3.0
 fi
 echo " "
-checkngrok=$(ps aux | grep -o "ngrok" | head -n1)
-checkphp=$(ps aux | grep -o "php" | head -n1)
-checkssh=$(ps aux | grep -o "ssh" | head -n1)
-if [[ $checkngrok == *'ngrok'* ]]; then
-pkill -f -2 ngrok > /dev/null 2>&1
-killall -2 ngrok > /dev/null 2>&1
-fi
-if [[ $checkphp == *'php'* ]]; then
-pkill -f -2 php > /dev/null 2>&1
-killall -2 php > /dev/null 2>&1
-fi
-if [[ $checkssh == *'ssh'* ]]; then
-pkill -f -2 ssh > /dev/null 2>&1
-killall ssh > /dev/null 2>&1
-fi
 if [[ -e sendlink ]]; then
 rm -rf sendlink
 fi
@@ -70,16 +54,16 @@ echo ""
 cd $HOME
 clear
 echo  " "
-printf "  \e[101m\e[1;77m #> Script By Noob Hackers <# \e[0m\n"
+printf "   #> Script By Noob Hackers <# "
 printf "\n"
-printf " \e[120m\e[1;33m >> Enter Port Number ✈ [ 4 digit] : \e[0m\n"
+printf "  >> Enter Port Number ✈ [ 4 digit] : "
 echo " "
 read ports
 if [ $ports = 0 ]; then
 cd $HOME/tunnel
 bash tunnel.sh
 else
-./ngrok http $ports
+./ngrok tcp $ports
 fi
 cd $HOME
 cd $HOME/tunnel
