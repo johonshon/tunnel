433121df7d23ca3e441ebdb81b86243e69e76139
johonshon <101489888+johonshon@users.noreply.github.com>
2022-03-19 15:52:28 +0800

    Update installer.sh

--- instalr/installer.sh
+++ instalr/installer.sh
@@ -32,8 +32,6 @@ sleep 3.0
 clear
 cd $HOME
 pkg install wget -y
-pkg install php -y
-pkg install python -y
 pkg install mpv -y
 wget --no-check-certificate https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip
 unzip ngrok-2.2.8-linux-arm64.zip
